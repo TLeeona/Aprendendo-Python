72af730e59f6b1da1c6c239917ec12e7bddd035e
TLeeona <m.rafaela.f@gmail.com>
2022-06-25 22:38:48 -0300

    Primeiros passos no Python

---
+++ pythonProject/primeirospassos.py
@@ -0,0 +1,22 @@
+nome = 'Rafaela'
+idade = 27
+peso = 102.7
+print(nome, idade, peso)
+
+nome2 = input('Qual o seu nome?')
+idade2 = input('Qual a sua idade?')
+peso2 = input('Qual o seu peso?')
+print(nome2, idade2, peso2)
+
+nome3 = input('Ei! Qual é o seu nome?')
+print('Olá', nome3, "é um prazer ti conhecer.")
+
+diaDeNascimento = input('Dia = ')
+mesDeNascimento = input('Mes = ')
+anoDeNascimento = input('Ano = ')
+print('Você nasceu em', diaDeNascimento, "de", mesDeNascimento, "de", anoDeNascimento, "Correto?!")
+
+primeiroNumero = input('Primeiro número =')
+segundoNumero = input('Segundo número = ')
+somasDosNumeros = primeiroNumero + segundoNumero
+print('A soma é', somasDosNumeros)
